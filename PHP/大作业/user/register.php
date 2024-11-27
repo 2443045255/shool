84c959f3b92552da1ac2e188ebeda6e5795a37b9
Rxaser <93034610+2443045255@users.noreply.github.com>
2024-11-27 09:49:56 +0800

    2024/11/27 周三 9:49:56.64

---
+++ PHP/大作业/user/register.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>register</title>
+</head>
+
+<body>
+    <div class="container">
+        <form action="">
+            <ul>
+                <li class="line">
+                    <span class="title">用户名:</span>
+                    <input type="text" name="username">
+                </li>
+            </ul>
+            <ul>
+                <li class="line">
+                    <span class="title">密码:</span>
+                    <input type="password" name="password">
+                </li>
+            </ul>
+            <ul>
+                <li class="line">
+                    <span class="title">确认密码:</span>
+                    <input type="password" name="re_password">
+                </li>
+            </ul>
+            <ul>
+                <li class="line">
+                    <input type="submit" name="提交注册">
+                    <a href="./login.php">已有账号，去登陆</a>
+                </li>
+            </ul>
+
+        </form>
+    </div>
+</body>
+
+</html>
