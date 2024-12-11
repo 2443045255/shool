98c71ad092e867c702c4bba2c4d8b013440dbe70
Rxaser <2443045255@qq.com93034610+2443045255@users.noreply.github.com>
2024-12-11 08:04:14 +0800

    2024/12/11 周三 8:04:14.21

--- JQuery/_大作业/奔驰/js/index.js
+++ JQuery/_大作业/奔驰/js/index.js
@@ -1,21 +1,35 @@
 var section_hight = 0
+var section_length = 0
+var mainScrollVal = 0
+
+document.body.onresize = function () {
+  修正滚动距离()
+}
+
 $(() => {
   section_hight = $('section').height()
-
+  section_length = $('section').length - 1
 })
+
 var main = document.querySelector('main')
-main.onresize = function () {
+var mainScrollIndex = 0
+
+
+
+function 修正滚动距离() {
   section_hight = $('section').height()
+  main.scrollTop = mainScrollIndex * section_hight
 }
-var bodyScrollIndex = 0
+
 var main_scrollActive = false
 main.addEventListener('mousewheel', function (event) {
   if (main_scrollActive) { return }
   if (event.deltaY > 0) {
+    if (main.scrollTop == section_hight * section_length) { return }
     main_scrollActive = true
-    bodyScrollIndex += section_hight
+    mainScrollIndex++
     main.scrollTo({
-      top: bodyScrollIndex,
+      top: mainScrollIndex * section_hight,
       left: 0,
       behavior: "smooth",
     });
@@ -23,9 +37,9 @@ main.addEventListener('mousewheel', function (event) {
   } else {
     if (main.scrollTop == 0) { return }
     main_scrollActive = true
-    bodyScrollIndex -= section_hight
+    mainScrollIndex--
     main.scrollTo({
-      top: bodyScrollIndex,
+      top: mainScrollIndex * section_hight,
       left: 0,
       behavior: "smooth",
     });
@@ -42,39 +56,37 @@ $(() => {
   $('.carousel_pre').click(() => {
     carousel('pre')
   })
-
-  var active_carousel_index = -100
-  var old_carousel_index = 1
-  function carousel(type) {
-    if (type == 'next') {
-      if (active_carousel_index == -500) {
-        $('.carousel_ul').css({
-          'transition': 'none',
-          'transform': 'translateX(0vw)'
-        })
-        document.body.offsetHeight
-        active_carousel_index = 0
-      }
-      active_carousel_index -= 100
-      $('.carousel_ul').css({
-        'transition': '.4s',
-      })
-      $('.carousel_ul').css('transform', `translateX(${active_carousel_index}vw)`)
-    }
-    if (type == 'pre') {
-      if (active_carousel_index == 0) {
-        $('.carousel_ul').css({
-          'transition': 'none',
-          'transform': 'translateX(-500vw)'
-        })
-        document.body.offsetHeight
-        active_carousel_index = -500
-      }
-      active_carousel_index += 100
-      $('.carousel_ul').css({
-        'transition': '.4s',
-      })
-      $('.carousel_ul').css('transform', `translateX(${active_carousel_index}vw)`)
-    }
-  }
 })
+var active_carousel_index = -100
+function carousel(type) {
+  if (type == 'next') {
+    if (active_carousel_index == -500) {
+      $('.carousel_ul').css({
+        'transition': 'none',
+        'transform': 'translateX(0vw)'
+      })
+      document.body.offsetHeight
+      active_carousel_index = 0
+    }
+    active_carousel_index -= 100
+    $('.carousel_ul').css({
+      'transition': '.4s',
+    })
+    $('.carousel_ul').css('transform', `translateX(${active_carousel_index}vw)`)
+  }
+  if (type == 'pre') {
+    if (active_carousel_index == 0) {
+      $('.carousel_ul').css({
+        'transition': 'none',
+        'transform': 'translateX(-500vw)'
+      })
+      document.body.offsetHeight
+      active_carousel_index = -500
+    }
+    active_carousel_index += 100
+    $('.carousel_ul').css({
+      'transition': '.4s',
+    })
+    $('.carousel_ul').css('transform', `translateX(${active_carousel_index}vw)`)
+  }
+}
