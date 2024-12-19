722f83d7963d0830e3ae0174c92ceed5034c6508
Rxaser <93034610+2443045255@users.noreply.github.com>
2024-12-19 17:38:29 +0800

    2024/12/19 周四17:38:29.71

--- PHP/大作业/user/login.php
+++ PHP/大作业/user/login.php
@@ -1,29 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<div class="container">
-        <form action="">
-            <ul>
-                <li class="line">
-                    <span class="title">用户名:</span>
-                    <input type="text" name="username">
-                </li>
-                <li class="line">
-                    <span class="title">密码:</span>
-                    <input type="password" name="password">
-                </li>
-                <li class="line">
-                    <input type="submit" value="提交登录">
-                    <a href="./register.php">去注册</a>
-                </li>
-            </ul>
 
-        </form>
-    </div>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>登录</title>
+  <link rel="stylesheet" href="../style/main.css">
+  <link rel="stylesheet" href="../style/login.css">
+</head>
+
+<body>
+  <div class="container">
+    <form action="">
+      <ul>
+        <div class="login-title">登录</div>
+        <li class="line line-input">
+          <span class="title">用户名:</span>
+          <input type="text" name="username" autocomplete="off">
+        </li>
+        <li class="line line-input">
+          <span class="title">密码:</span>
+          <input type="password" name="password" autocomplete="off">
+        </li>
+        <li class="line">
+          <input type="submit" value="提交登录" class="submit_btn">
+          <a href="./register.php" class="topage">去注册</a>
+        </li>
+      </ul>
+
+    </form>
+  </div>
 </body>
+
 </html>
--- PHP/大作业/user/register.php
+++ PHP/大作业/user/register.php
@@ -2,35 +2,38 @@
 <html lang="zh-cn">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>register</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>注册</title>
+  <link rel="stylesheet" href="../style/main.css">
+  <link rel="stylesheet" href="../style/login.css">
 </head>
 
 <body>
-    <div class="container">
-        <form action="">
-            <ul>
-                <li class="line">
-                    <span class="title">用户名:</span>
-                    <input type="text" name="username">
-                </li>
-                <li class="line">
-                    <span class="title">密码:</span>
-                    <input type="password" name="password">
-                </li>
-                <li class="line">
-                    <span class="title">确认密码:</span>
-                    <input type="password" name="re_password">
-                </li>
-                <li class="line">
-                    <input type="submit" name="提交注册">
-                    <a href="./login.php">已有账号，去登陆</a>
-                </li>
-            </ul>
+  <div class="container">
+    <form action="">
+      <ul>
+        <div class="login-title">注册</div>
+        <li class="line line-input">
+          <span class="title">用户名:</span>
+          <input type="text" name="username" autocomplete="off">
+        </li>
+        <li class="line line-input">
+          <span class="title">密码:</span>
+          <input type="password" name="password" autocomplete="off">
+        </li>
+        <li class="line line-input">
+          <span class="title">确认密码:</span>
+          <input type="password" name="re_password">
+        </li>
+        <li class="line">
+          <input type="submit" name="提交注册" class="submit_btn">
+          <a href="./login.php" class="topage">已有账号，去登陆</a>
+        </li>
+      </ul>
 
-        </form>
-    </div>
+    </form>
+  </div>
 </body>
 
 </html>
