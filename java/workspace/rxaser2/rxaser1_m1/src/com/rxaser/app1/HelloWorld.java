8785c187af73135cb6df5581e9ef788f05ab9371
2443045255 <2443045255@qq.com93034610+2443045255@users.noreply.github.com>
2025-02-24 12:07:32 +0800

    2025/02/24 周一12:07:32.16

---
+++ java/workspace/rxaser2/rxaser1_m1/src/com/rxaser/app1/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.rxaser.app1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("你好世界");
+    }
+}
--- node/app.js
+++ node/app.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+const fs = require('fs')
+var data = "你好世界"
+
+try {
+  fs.writeSync('./hello.txt', data)
+} catch (error) {
+  console.log("写入失败" + error);
+
+}
+
+
+/* fs.writeFile('./hello.txt', data, (err) => {
+  if (err) throw err;
+  console.log("文件写入完成");
+}) */
