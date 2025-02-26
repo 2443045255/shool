502c8afd85b93de7995c0b06da754e576215a9de
2443045255 <2443045255@qq.com93034610+2443045255@users.noreply.github.com>
2025-02-26 09:59:48 +0800

    2025/02/26 周三 9:59:48.67

---
+++ java/workspace/rxaser2/rxaser1_m1/src/com/rxaser/app1/CLass2.java
@@ -0,0 +1,8 @@
+package com.rxaser.app1;
+
+public class CLass2 {
+    public static void main(String[] args) {
+        System.out.println("这是Class2");
+        System.out.println("CLass2.main");
+    }
+}
