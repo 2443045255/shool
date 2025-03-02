b3f770fa4809e0624ae34a5a8cf25a6eafbe29b5
2443045255 <93034610+2443045255@users.noreply.github.com>
2025-03-02 18:57:02 +0800

    2025/03/02 周日18:57:02.66

--- C语言/keyCon.cpp
+++ C语言/keyCon.cpp
@@ -17,14 +17,18 @@ void keyup(int vk)
 void keypress(int vk)
 {
 	keydown(vk);
+	Sleep(10);
 	keyup(vk);
 }
 
-void moreTime(int vk,int Mtime,int Stime = 200){
+void moreTime(int vk,int Mtime,int Stime){
 	if(Mtime){
-		for(int i=0;i<=Mtime;i++){
+		for(int i=1;i<=Mtime;i++){
 			keypress(vk);
 			Sleep(Stime);
+			if(i == Mtime){
+				printf("已完成(%d,%d,%d)\n",vk,Mtime,Stime);
+			}
 		}
 	}
 }
@@ -33,14 +37,55 @@ int main()
 {
 	SetConsoleTitle("");
 	printf("keyInput:\n");
-	char u_vk[16],u_Mtime[16],u_Stime;
+	int u_vk;
+	int u_Mtime,u_Stime;
+	int old_vk;
 	int isExit = true;
 	int whileTime = 0;
 	
 	while (isExit){
-		scanf("%s%s%s", &u_vk,&u_Mtime,&u_Stime);
-		printf("u_Mtime=%s,u_vk=%s,u_Stime=%d\n",u_Mtime,u_vk,u_Stime);
+		u_vk = 0;
+		scanf("%d",&u_vk);
+		
+		if(u_vk == 0){
+			scanf("%s",&u_vk);
+		}
+		
+		if(u_vk >= 'a' && u_vk <= 'z'){
+			u_vk = u_vk - 32;
+		}
+//		if(u_vk >= 'A' && u_vk <= 'Z'){
+//			
+//		}
+		printf("u_vk=%d",u_vk);
+		scanf("%c",&u_Mtime);
+		
+		if(u_Mtime == 32){
+			scanf("%d",&u_Mtime);
+		}
+		
+		if(u_Mtime == 10){
+			u_Mtime = 1;
+			u_Stime = 100;
+		}else{
+			scanf("%c",&u_Stime);
+			
+			if(u_Stime == 32){
+				scanf("%d",&u_Stime);
+			}
+			
+			if(u_Stime == 10){
+				u_Stime = 100;
+			}
+		}
+		
+		printf("u_vk(按键)=%d,u_Mtime(次数)=%d,u_Stime(延迟)=%d\n",u_vk,u_Mtime,u_Stime);
+		
+		moreTime(u_vk,u_Mtime,u_Stime);
+		
 		fflush(stdin);
+		
+		
 //		if (userInput == '0'){
 //			isExit = false;
 //		}if (userInput >= 'a' && userInput <= 'z'){
---
+++ C语言/keyCon测试.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	int u_vk;
+	
+	scanf("%c", &u_vk);
+	printf("u_vk=%d",u_vk);
+	
+	return 0;
+}
