d2d6ae9b4e5e9ddf1fea94960f62802871e2f01d
2443045255 <2443045255@qq.com93034610+2443045255@users.noreply.github.com>
2025-03-18 10:27:29 +0800

    2025/03/18 周二10:27:29.04

--- Node/http请求/app.js
+++ Node/http请求/app.js
@@ -1,37 +1,40 @@
 const http = require('http');
 
-function httpTo(ip3) {
-    for (let ip4 = 1; ip4 <= 254; ip4++) {
-        const options = {
-            hostname: `10.240.${ip3}.${ip4}`,
-            port: 23333,
-            path: '/',
-            method: 'GET'
-        };
 
-        const req = http.request(options, res => {
-            console.log(`http://${options.hostname}:${options.port} 状态码: ${res.statusCode}`);
-            res.on('data', d => {
-                // process.stdout.write(d);
-            });
-        });
+var Uport = 3000
+var ip3 = 200
+var num = 0
+setInterval(() => {
+  for (let ip4 = 1; ip4 <= 254; ip4++) {
+    const options = {
+      hostname: `10.240.${ip3}.${ip4}`,
+      port: Uport,
+      path: '/',
+      method: 'GET'
+    };
+    
+    const req = http.request(options, res => {
+      console.log(`http://${options.hostname}:${options.port} 状态码: ${res.statusCode}-${num}`);
+      res.on('data', d => {
+        // process.stdout.write(d);
+      });
+    });
 
-        req.on('error', error => {
-            // console.error('请求错误:', error);
-        });
+    req.on('error', error => {
+      // console.error('请求错误:', error);
+    });
 
-        var timeout = 100;
-        var timeoutId = setTimeout(() => {
-            // console.error(`${options.hostname}请求超时`);
-            req.abort();
-        }, timeout);
+    var timeout = 1000;
+    var timeoutId = setTimeout(() => {
+      // console.error(`${options.hostname}请求超时`);
+      req.abort();
+    }, timeout);
 
-        req.on('response', () => {
-            timeoutId = null
-        });
+    req.on('response', () => {
+      timeoutId = null
+    });
 
-        req.end();
-    }
-}
-
-httpTo(144)
+    req.end();
+  }
+  num++
+}, 2000);
--- nodejs/http/02req.js
+++ nodejs/http/02req.js
@@ -2,20 +2,29 @@ const http = require("http")
 const path = require("path")
 const fs = require("fs")
 
+const post = 3000
+
 const httpServer = http.createServer()
 
 httpServer.on("request", (req, res) => {
+  res.writeHead(200, {
+    "content-type": 'text/html;charset=utf8'
+  })
   var fileUrl = null
   if (!path.extname(req.url)) {
     fileUrl = `.${req.url}/index.html`
   } else {
     fileUrl = `.${req.url}`
   }
-  fs.readFile(path.join(__dirname, fileUrl), 'utf-8', (err, data) => {
-    res.end(data)
+  fs.readFile(path.join(__dirname, "./htmlFile/" + fileUrl), 'utf-8', (err, data) => {
+    if (err) {
+      res.end("路径不存在")
+    } else {
+      res.end(data)
+    }
   })
 })
 
-httpServer.listen(801, () => {
-  console.log("http://127.0.0.1:801");
+httpServer.listen(post, () => {
+  console.log("http://localhost:" + post);
 })
