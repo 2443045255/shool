e57b9f4a569f86ad7aae2ee6dba6480ee2479067
2443045255 <2443045255@qq.com93034610+2443045255@users.noreply.github.com>
2025-03-19 11:03:38 +0800

    2025/03/19 周三11:03:38.61

--- java/workspace/rxaser2/rxaser1_m1/src/com/rxaser/app1/test1.java
+++ java/workspace/rxaser2/rxaser1_m1/src/com/rxaser/app1/test1.java
@@ -19,16 +19,19 @@ public class test1 {
 //            System.out.println(num1 + "等于" + num2);
 //        }
 
-        System.out.print("输入成绩:");
-        float num1 = sc.nextFloat();
-        if (num1 >= 90 && num1 <= 100) {
-            System.out.println("优秀");
-        } else if (num1 >= 80) {
-            System.out.println("良");
-        } else if (num1 >= 70) {
-            System.out.println("中");
-        } else if (num1 >= 60) {
-            System.out.println("及格");
-        }
+//        System.out.print("输入成绩:");
+//        float num1 = sc.nextFloat();
+//        if (num1 >= 90 && num1 <= 100) {
+//            System.out.println("优秀");
+//        } else if (num1 >= 80) {
+//            System.out.println("良");
+//        } else if (num1 >= 70) {
+//            System.out.println("中");
+//        } else if (num1 >= 60) {
+//            System.out.println("及格");
+//        }
+        int a = 10;
+        int m = 5;
+        System.out.println(m = ++a * m);
     }
 }
