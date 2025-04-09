a083b6c28901848d073a393a04e17b3d72acc872
2443045255 <2443045255@qq.com93034610+2443045255@users.noreply.github.com>
2025-04-09 16:50:21 +0800

    2025/04/09 周三16:50:21.59

---
+++ Vuejs_2/vue2_1/src(自定义事件 全局事件总线)/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <MySchool1 />
+    <MyStudent />
+  </main>
+</template>
+
+<script>
+import MySchool1 from './components/MySchool1.vue';
+import MyStudent from './components/MyStudent.vue';
+
+export default {
+  name: "App",
+  components: {
+    MySchool1,
+    MyStudent
+  },
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  },
+  //监听
+  watch: {
+
+  }
+}
+</script>
+
+<style></style>
---
+++ Vuejs_2/vue2_1/src/components/ListView.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul class="todo-main">
+    <ItemView v-for="todoObj in todos" :key="todoObj.id" :todoObj="todoObj" />
+  </ul>
+</template>
+
+<script>
+import ItemView from "./ItemView.vue";
+
+export default {
+  name: "ListView",
+  components: {
+    ItemView,
+  },
+  props: ["todos"],
+};
+</script>
+
+<style scoped>
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
